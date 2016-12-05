845684285472bd856bcb6d0ff91c33bf3d5150b2
Kevin Velasco <me@kvnvelasco.com>
2016-12-05 18:59:07 +0800

    initial commit

---
+++ app/routes.js
@@ -0,0 +1,9 @@
+// @flow
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import Home from './screens/home'
+
+export default (
+  <Route path="/" component={Home}>
+  </Route>
+);
